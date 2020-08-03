a96e51f3c4c785fd200c1a7dabbfc2e497bb4759
samuelematias <hi@samuelematias.com>
2020-08-03 17:59:54 -0300

    feat(screen): improving home_view responsive

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_architecture/responsive/orientation_layout.dart';
+import 'package:responsive_architecture/responsive/screen_type_layout.dart';
+import 'package:responsive_architecture/views/home/home_view_mobile.dart';
+import 'package:responsive_architecture/views/home/home_view_tablet.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
+    return ScreenTypeLayout(
+      mobile: OrientationLayout(
+        portrait: HomeMobilePortrait(),
+        landscape: HomeMobileLandscape(),
+      ),
+      tablet: HomeTablet(),
     );
   }
 }
---
+++ lib/views/home/home_view_mobile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_architecture/widgets/app_drawer/app_drawer.dart';
+
+class HomeMobilePortrait extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: AppDrawer(),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: IconButton(
+              icon: Icon(
+                Icons.menu,
+                size: 30,
+              ),
+              onPressed: () {
+                _scaffoldKey.currentState.openDrawer();
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomeMobileLandscape extends StatelessWidget {
+  const HomeMobileLandscape({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          AppDrawer(),
+        ],
+      ),
+    );
+  }
+}
